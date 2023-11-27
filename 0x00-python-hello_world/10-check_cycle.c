ca5301c147d0189a70ef8cfb20ab05d448fe06f7
Meriem1551 <meriem200409@gmail.com>
2023-11-27 14:08:01 -0800

    solving task 10

---
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+
+/**
+ * check_cycle - checks if there si a cycle
+ * @list: pointer to a list
+ * Return: 1 if there is 0 otherwise
+ */
+int check_cycle(listint_t *list)
+{
+	listint_t *tmp;
+	if (list == NULL)
+		return (0);
+	tmp = list;
+	while (tmp != NULL)
+	{
+		if (tmp->next == list)
+			return (1);
+		tmp = tmp->next;
+	}
+	return (0);
+}
