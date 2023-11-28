691facfa9c94abc5a20e38df3595fac8d5e2a0dd
Meriem1551 <meriem200409@gmail.com>
2023-11-28 13:49:19 -0800

    solving task 9

--- 0x01-python-if_else_loops_functions/9-print_last_digit.py
+++ 0x01-python-if_else_loops_functions/9-print_last_digit.py
@@ -3,8 +3,5 @@ def print_last_digit(number):
 
     if number < 0:
         number = -number
-        print(-(number % 10), end = "")
-        return -(number % 10)
-    else:
-        print(number % 10, end = '')
-        return number % 10
+    print(number % 10, end = '')
+    return number % 10
