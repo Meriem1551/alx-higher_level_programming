c3bb628d1b8efe1db19bd18177d3b33ea5a5b455
Meriem1551 <meriem200409@gmail.com>
2023-11-28 14:02:52 -0800

    solving task 12

--- 0x01-python-if_else_loops_functions/12-fizzbuzz.py
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def fizzbuzz():
     for i in range(1, 101):
-        if (i % 3 == 0 and i % 5 == 0:
+        if i % 3 == 0 and i % 5 == 0:
                 print("FizzBuzz", end='')
         elif i % 3 != 0 and i % 5 != 0:
             print(i, end='')
