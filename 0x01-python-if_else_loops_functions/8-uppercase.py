7ec74440c17967ed3786e7d1c45203be0cde8a0a
Meriem1551 <meriem200409@gmail.com>
2023-11-28 13:23:41 -0800

    solving task 8

---
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+def uppercase(str):
+    for i in range(len(str)):
+        char = ord(str[i])
+        if char >= 97 and char <= 122:
+            char = char - 32
+            print("{}".format(chr(char)), end='');
+    print()
