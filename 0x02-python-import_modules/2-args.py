d77054c44f274fe8ccbec2ea8ffeb496b0c13ca0
Meriem1551 <meriem200409@gmail.com>
2023-11-30 09:58:16 -0800

    solving task 2

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -5,7 +5,7 @@ if __name__ == "__main__":
         print("0 arguments.")
     else:
         print("{:d} argument".format(len(sys.argv) - 1), end="")
-        if num_args > 2:
+        if len(sys.argv) > 2:
             print("s", end="")
             print(":")
             for i in range(1, len(sys.argv)):
