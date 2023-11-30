2e9dfe20d3dc5ee7056b1a0827feece841e8bd1e
Meriem1551 <meriem200409@gmail.com>
2023-11-30 08:45:45 -0800

    solving task 0

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 from add_0 import add
-if "__name__" == "__main__":
+
+
+if __name__ == "__main__":
     a = 1
     b = 2
     print("{:d} + {:d} = {:d}".format(a, b, add(a, b)))
