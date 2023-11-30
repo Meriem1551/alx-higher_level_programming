9747798659b81fd26b56607865a9a32c495218e6
Meriem1551 <meriem200409@gmail.com>
2023-11-30 10:51:58 -0800

    solving task 6

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
+import sys
+from calculator_1 import add, sub, mul, div
 if __name__ == "__main__":
-    import sys
-    from calculator_1 add, sub, mul, div
     num_args = len(sys.argv) - 1
     if num_args != 3:
         print("./100-my_calculator.py <a> <operator> <b>")
