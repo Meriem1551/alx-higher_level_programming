cacb81de89ca64221de5ee254c64b7bbd0261805
Meriem1551 <meriem200409@gmail.com>
2023-11-30 10:14:42 -0800

    fixing some errors

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -8,5 +8,5 @@ if __name__ == "__main__":
         if len(sys.argv) > 2:
             print("s", end="")
             print(":")
-            for i in range(1, len(sys.argv) + 1):
+            for i in range(1, len(sys.argv)):
                 print("{:d}: {:s}".format(i, sys.argv[i]))
