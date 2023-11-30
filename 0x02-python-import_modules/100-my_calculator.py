7e4ec636f3312e5f53141d0b26a3ac95e7e271f6
Meriem1551 <meriem200409@gmail.com>
2023-11-30 10:47:07 -0800

    solving task 6

---
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    from calculator_1 import add, sub, mul, div
+    import sys
+    num_args = len(sys.argv) - 1
+    if num_args != 3:
+        print("./100-my_calculator.py <a> <operator> <b>")
+        exit(1)
+    elif sys.argv[2] != "+" and sys.argv[2] != "-" and sys.argv[2] != "*" and sys.argv[2] != "/":
+        print("Unknown operator. Available operators: +, -, * and /")
+        exit(1)
+    else:
+        a = int(sys.argv[1])
+        b = int(sys.argv[3])
+        if sys.argv[2] == "+":
+            print("{:d} + {:d} = {:d}".format(a, b, add(a, b)))
+        elif sys.argv[2] == "-":
+            print("{:d} - {:d} = {:d}".format(a, b, sub(a, b)))
+        elif sys.argv[2] == "*":
+            print("{:d} * {:d} = {:d}".format(a, b, mul(a, b)))
+        else:
+            print("{:d} / {:d} = {:d}".format(a, b, div(a, b)))
