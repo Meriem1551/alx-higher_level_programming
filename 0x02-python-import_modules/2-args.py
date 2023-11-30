8a70d022004be4f24b2efc18a980d1166f7a9a98
Meriem1551 <meriem200409@gmail.com>
2023-11-30 09:57:10 -0800

    solving task 2

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,13 +1,12 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
     import sys
-    num_args = len(sys.argv)
-    if num_args == 1:
+    if len(sys.argv) == 1:
         print("0 arguments.")
     else:
-        print("{:d} argument".format(num_args - 1), end="")
+        print("{:d} argument".format(len(sys.argv) - 1), end="")
         if num_args > 2:
             print("s", end="")
             print(":")
-            for i in range(1, num_args):
+            for i in range(1, len(sys.argv)):
                 print("{:d}: {:s}".format(i, sys.argv[i]))
