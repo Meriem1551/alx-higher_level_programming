0281069b03e7a8f47471c0da0d4d5c12137af933
Meriem1551 <meriem200409@gmail.com>
2023-11-30 09:44:33 -0800

    solving task 2

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,16 +1,13 @@
 #!/usr/bin/python3
-import sys
-
-if __name__ != "__main__":
-    exit()
-
-num_args = len(sys.argv) - 1
-if num_args == 0:
-    print("0 arguments.")
-elif num_args == 1:
-    print("1 argument:")
-    print("1: " + sys.argv[1])
-else:
-    print("{:d} arguments:".format(num_args))
-    for i in range(1, num_args):
-        print(f"{i}: {sys.argv[i]}")
+if __name__ == "__main__":
+    import sys
+    num_args = len(sys.argv) - 1
+    if num_args == 0:
+        print("0 arguments.")
+    elif num_args == 1:
+        print("1 argument:")
+        print("1: {:s}".format(sys.argv[1]))
+    else:
+        print("{:d} arguments:".format(num_args))
+        for i in range(1, num_args):
+            print("{:d}: {:s}".format(i, sys.argv[i]))
