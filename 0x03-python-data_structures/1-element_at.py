6c56966f5f03e4629539fbf84c90625c3a059e1f
Meriem1551 <meriem200409@gmail.com>
2023-12-03 21:51:15 -0800

    solving task 1

---
+++ 0x03-python-data_structures/1-element_at.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def element_at(my_list, idx):
+    if idx < 0:
+        return None
+    elif i > my_list.len():
+        return None
+    else:
+        return my_list[idx]
+            
+
