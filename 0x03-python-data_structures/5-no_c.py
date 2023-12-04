92fe671001ee968c38192f20c75f9e7bf663936d
Meriem1551 <meriem200409@gmail.com>
2023-12-04 09:04:07 -0800

    solving task 5

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -3,4 +3,4 @@ def no_c(my_string):
     if my_string is None:
         return None
     new_string = ''.join([char for char in my_string if char != 'c' or char != 'C'])
-    print(new-string)
+    return new-string
