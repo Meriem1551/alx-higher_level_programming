37d78109880aa4fecfbb254672d062063842d487
Meriem1551 <meriem200409@gmail.com>
2023-12-03 21:41:01 -0800

    solvig task 0

--- 0x03-python-data_structures/0-print_list_integer.py
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -4,6 +4,3 @@ if __name__ == "__main__":
         i = 0
         for n in my_list:
             print("{:d}".format(n))
-            i += 1
-            if i != my_list.len():
-                print("\n")
