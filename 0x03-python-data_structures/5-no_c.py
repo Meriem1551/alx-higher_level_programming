55fee648b7ab6bdec962d7e117373e0ca65b191c
Meriem1551 <meriem200409@gmail.com>
2023-12-04 09:15:29 -0800

    solving task 5

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,5 +1,9 @@
-#!/usr/bin/env python3
+#!/usr/bin/python3
 def no_c(my_string):
-    my_string = ''.join([char for char in my_string if char != 'c'])
-    my_string = ''.join([char for char in my_string if char != 'C'])
-    return my_string
+    new_str = ""
+    for i in range(len(my_string)):
+        if my_string[i] == 'C' or my_string[i] == 'c':
+            pass
+        else:
+            new_str += my_string[i]
+    return new_str
