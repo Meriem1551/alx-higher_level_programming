0e1f70c49146dc813640ff336832676c1dfc9c78
Meriem1551 <meriem200409@gmail.com>
2023-12-04 09:40:28 -0800

    solving task 6

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
-    for row in matrix:
-        for i in range(len(row)):
-            print("{:d}".format(row[i]), end="")
-        print("\n")
+    if matrix = None:
+        print(None)
+    else:
+        for row in matrix:
+            for i in range(len(row)):
+                print("{:d} ".format(row[i]), end="")
