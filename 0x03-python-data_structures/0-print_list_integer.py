3bd65e9303a9ebe58289c068dd7748c1702681dc
Meriem1551 <meriem200409@gmail.com>
2023-12-03 21:44:23 -0800

    solvig task 0

--- 0x03-python-data_structures/0-print_list_integer.py
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
-if __name__ == "__main__":
-    def print_list_integer(my_list=[]):
-        for n in my_list:
-            print("{:d}".format(n))
+def print_list_integer(my_list=[]):
+    for n in my_list:
+        print("{:d}".format(n))
