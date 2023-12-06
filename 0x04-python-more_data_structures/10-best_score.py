753eb89a36dfd2ca6a3c8755a3ea202371348c5d
Meriem1551 <meriem200409@gmail.com>
2023-12-06 09:29:25 -0800

    solving task 10

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 def best_score(a_dictionary):
+    if not a_dictionary:
+        return None
     if a_dictionary is not None:
         max_score = max(a_dictionary.values())
         for key, value in a_dictionary.items():
