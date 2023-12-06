9dea39e3af7437463884a31edffdb87bb4312d4c
Meriem1551 <meriem200409@gmail.com>
2023-12-06 09:20:55 -0800

    fixi,g python style

--- 0x04-python-more_data_structures/9-multiply_by_2.py
+++ 0x04-python-more_data_structures/9-multiply_by_2.py
@@ -2,5 +2,5 @@
 def multiply_by_2(a_dictionary):
     new_dict = {}
     for key, value in a_dictionary.items():
-            new_dict[key] = value * 2
+        new_dict[key] = value * 2
     return new_dict
