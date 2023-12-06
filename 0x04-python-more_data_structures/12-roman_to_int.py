0b844d905284a65ab62ad9a6a06cef530b784442
Meriem1551 <meriem200409@gmail.com>
2023-12-06 10:06:05 -0800

    solving task 12

---
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+def roman_to_int(roman_string):
+    if not roman_string  or roman_string is None:
+        return 0
+    roman_nums = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
+    result = 0
+    last_rom = 0
+    for num in reversed(roman_string):
+        value = roman_nums[num]
+        if last_rom > value:
+            result -= value
+        else:
+            result += value
+        last_rom = value
+    return result
