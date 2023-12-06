bae89a8089e5f943f559aa4272659d925922f846
Meriem1551 <meriem200409@gmail.com>
2023-12-06 09:28:13 -0800

    solving task 10

---
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+def best_score(a_dictionary):
+    if a_dictionary is not None:
+        max_score = max(a_dictionary.values())
+        for key, value in a_dictionary.items():
+            if value == max_score:
+                return key
