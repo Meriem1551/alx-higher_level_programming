4b44006c19ce844eb0e7afab7ba256a32c2f9a42
Meriem1551 <meriem200409@gmail.com>
2023-12-05 21:23:25 -0800

    solving task 1

---
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+def search_replace(my_list, search, replace):
+    new_list = my_list[:]
+    for i in range(len(my_list)):
+        if new_list[i] == search:
+            new_list[i] = replace
+    return new_list
