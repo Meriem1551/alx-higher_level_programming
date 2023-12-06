68f9a7097c8958d2ffa86a818ccb86be16140fd8
Meriem1551 <meriem200409@gmail.com>
2023-12-06 09:44:20 -0800

    solving size problem

--- 0x04-python-more_data_structures/11-multiply_list_map.py
+++ 0x04-python-more_data_structures/11-multiply_list_map.py
@@ -1,7 +1,3 @@
 #!/usr/bin/python3
 def multiply_list_map(my_list=[], number=0):
-    if not my_list:
-        return None
-    if my_list is not None:
-        new_list = list(map(lambda x: x * number, my_list))
-    return new_list
+    return(list(map(lambda x: x * number, my_list)))
